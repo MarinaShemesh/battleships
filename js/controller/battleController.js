4612f222c2fc947ee2fe8422979e5f3f441b2152
MarinaShemesh <marina.shemesh1@gmail.com>
2018-01-07 22:52:46 +0200

    100daysofcode_7jan

--- js/controller/battleController.js
+++ js/controller/battleController.js
@@ -10,4 +10,13 @@
               mheading:"A '100 days of coding' project",
               subheading: "Battleships: HMS Shoot them all"
             };
+
+        vm.playSound = function(){
+          const audio = new Audio('./sounds/beach.wav');
+          audio.play();
+        };
+
+        vm.showImage = function(){
+          vm.show = true;
+        }
       }
