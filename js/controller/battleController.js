b720d117e2ed96a90eb180f405318762e0c8c340
MarinaShemesh <marina.shemesh1@gmail.com>
2018-01-08 13:12:53 +0200

    8jan

--- js/controller/battleController.js
+++ js/controller/battleController.js
@@ -1,9 +1,9 @@
      angular.module('battleshipsApp')
              .controller('battleController', battleController);
      
-     battleController.$inject = ['$scope'];
+     battleController.$inject = ['$scope', '$timeout'];
                                   
-      function battleController ($scope){
+      function battleController ($scope, $setTimeout){
         const vm = this;
 
          vm.headings = {
@@ -11,12 +11,22 @@
               subheading: "Battleships: HMS Shoot them all"
             };
 
+      vm.showImage = false;
+
+      vm.hitTaken = function(){
+        vm.showImage = true;
+        $setTimeout(function() {
+         vm.showImage = false
+        }, 1000);
+
+        
+      }
+      
+
         vm.playSound = function(){
           const audio = new Audio('./sounds/beach.wav');
           audio.play();
         };
 
-        vm.showImage = function(){
-          vm.show = true;
-        }
-      }
+    
+    }
